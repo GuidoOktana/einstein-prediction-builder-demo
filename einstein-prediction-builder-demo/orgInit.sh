778bc44c942c22a7e091c31cafd272798cb64033
GuidoOktana <guido@oktana.io>
2020-01-13 13:42:15 -0300

    test script

--- einstein-prediction-builder-demo/orgInit.sh
+++ einstein-prediction-builder-demo/orgInit.sh
@@ -1,6 +1,15 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
-sfdx force:source:push
-sfdx force:user:password:generate
+# sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
+# sfdx force:source:push
+# sfdx force:user:password:generate
 
+# sfdx force:user:permset:assign -n EPB
+# sfdx force:org:open -p /lightning/setup/SetupOneHome/home
+
+
+sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix crash --userdomain course.org
+sfdx force:source:push
 sfdx force:user:permset:assign -n EPB
-sfdx force:org:open -p /lightning/setup/SetupOneHome/home
+# sfdx force:data:tree:import -p data/masterImportPlan.json
+# sfdx force:apex:execute -f scripts/setup.cls
+sfdx shane:user:password:set -p sfdx1234 -g User -l User
+sfdx force:org:open

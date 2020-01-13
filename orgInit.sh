fcf29f2c98207c2ec9ee441f9126878c3c9f1319
GuidoOktana <guido@oktana.io>
2020-01-13 14:35:47 -0300

    project struct

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,15 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s	
+# sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
+# sfdx force:source:push
+# sfdx force:user:password:generate
+# sfdx force:user:permset:assign -n EPB
+# sfdx force:org:open -p /lightning/setup/SetupOneHome/home
+
+
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1 
 sfdx force:source:push
 sfdx force:user:permset:assign -n EPB
+# sfdx force:data:tree:import -p data/masterImportPlan.json
+# sfdx force:apex:execute -f scripts/setup.cls
+
+sfdx force:user:password:generate
+sfdx force:org:open

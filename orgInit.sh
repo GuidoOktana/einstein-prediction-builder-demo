3ddb18b50345832d2baeeb44defa8d92e8bcb486
GuidoOktana <guido@oktana.io>
2020-01-14 10:36:43 -0300

    a lot

--- orgInit.sh
+++ orgInit.sh
@@ -7,9 +7,14 @@
 
 sfdx force:org:create -f config/project-scratch-def.json -s -d 1 
 sfdx force:source:push
-sfdx force:user:permset:assign -n EPB
-# sfdx force:data:tree:import -p data/masterImportPlan.json
+# sfdx force:user:permset:assign -n EPB
+
 # sfdx force:apex:execute -f scripts/setup.cls
+
+# sfdx force:data:bulk:upsert -s Contact -f ./data/contacts.csv -i Id -w 2
+
+sfdx force:data:tree:import -p data/accounts.json
+sfdx force:data:tree:import -p data/contacts.json
 
 sfdx force:user:password:generate
 sfdx force:org:open

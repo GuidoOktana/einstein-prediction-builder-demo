aaae5e8cefbfc31c1bb745c6f5900cf3245f2ff9
GuidoOktana <guido@oktana.io>
2020-01-14 14:20:26 -0300

    scripts change

--- orgInit.sh
+++ orgInit.sh
@@ -7,13 +7,12 @@ sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 
 # sfdx force:org:create -f config/project-scratch-def.json -s -d 1 
 sfdx force:source:push
-sfdx force:user:permset:assign -n EPB
-sfdx shane:data:tree:import -p data/plan.json -d data/
 
 # sfdx force:apex:execute -f scripts/setup.cls
 
 sfdx force:data:bulk:upsert -s Account -f ./data/accounts.csv -i Id -w 2
 sfdx force:data:bulk:upsert -s Contact -f ./data/contacts.csv -i Id -w 2
+sfdx force:user:permset:assign -n EPB
 
 # sfdx force:data:tree:import -p data/accounts.json
 # sfdx force:data:tree:import -p data/contacts.json

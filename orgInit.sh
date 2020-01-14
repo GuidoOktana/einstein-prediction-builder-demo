599ff5815d5d7a0f69428829d8495d4a1eb4f683
GuidoOktana <guido@oktana.io>
2020-01-14 13:42:09 -0300

    scripts updated

--- orgInit.sh
+++ orgInit.sh
@@ -1,22 +1,23 @@
-# sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 # sfdx force:source:push
 # sfdx force:user:password:generate
 # sfdx force:user:permset:assign -n EPB
 # sfdx force:org:open -p /lightning/setup/SetupOneHome/home
 
 
-sfdx force:org:create -f config/project-scratch-def.json -s -d 1 
+# sfdx force:org:create -f config/project-scratch-def.json -s -d 1 
 sfdx force:source:push
-# sfdx force:user:permset:assign -n EPB
+sfdx force:user:permset:assign -n EPB
 
 # sfdx force:apex:execute -f scripts/setup.cls
 
-# sfdx force:data:bulk:upsert -s Contact -f ./data/contacts.csv -i Id -w 2
+sfdx force:data:bulk:upsert -s Account -f ./data/accounts.csv -i Id -w 2
+sfdx force:data:bulk:upsert -s Contact -f ./data/contacts.csv -i Id -w 2
 
 # sfdx force:data:tree:import -p data/accounts.json
 # sfdx force:data:tree:import -p data/contacts.json
 
-sfdx shane:data:tree:import -p data/plan.json -d data/
+# sfdx shane:data:tree:import -p data/plan.json -d data/
 
 
 sfdx force:user:password:generate

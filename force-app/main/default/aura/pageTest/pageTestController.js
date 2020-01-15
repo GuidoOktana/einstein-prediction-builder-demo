8ec4778937a694c7d544e3200d1aa2fb4856a571
GuidoOktana <guido@oktana.io>
2020-01-15 13:58:57 -0300

    custom objects

---
+++ force-app/main/default/aura/pageTest/pageTestController.js
@@ -0,0 +1,63 @@
+({
+    doInit: function(component, event, helper) {
+        var developerName = component.get('v.developerName');
+        developerName = developerName.split('__')[1];
+        helper.listDashboards(component, developerName, function(err, dashboards) {
+            console.warn('dashboards: ', dashboards); 
+            if (dashboards) {
+                helper.describeDashboard(component, dashboards[0].id, function(err, dashboard) {
+                    component.set('v.dashboardDef', dashboard);
+                    helper.generatePageNav(component);                    
+                });
+            }
+        });
+        
+        var filter = {
+            datasets:{
+                "Regional_Sales":[
+                    {
+                        fields:[
+                            "Billing_Country"
+                        ],
+                        filter:{
+                            operator:"in",
+                            values:[
+                                "USA"
+                            ]
+                        }
+                    }
+                ]
+            }
+        };
+        
+        
+        var json = JSON.stringify(filter, null, 2);
+        component.set('v.filter', json);         
+    },
+    
+    handleTabSelect: function(component, event, helper) {
+        console.warn('handleTabSelect: ', event, event.getSource());
+        var params = event.getParams();
+        var pageId = params.id;
+        console.warn('pageId: ', pageId);
+        
+        var filter = component.get('v.filter');
+        var developerName = component.get('v.developerName');
+        console.warn("developerName: ", developerName);
+        console.warn("pageId: ", pageId);
+        var evt = $A.get('e.wave:update');
+        evt.setParams({
+            value: filter,
+            id: developerName,
+            pageid: pageId,
+            type: "dashboard"
+        });
+        evt.fire();
+    },
+
+    handleSelectionChanged: function (component, event, helper) {
+        console.warn('handleSelectionChanged: ', event.getParams(), JSON.stringify(event.getParams(), null, 2));
+        
+        
+    }    
+})
